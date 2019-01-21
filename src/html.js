b918d218559965edf5318ef47ab64f5622a86486
omrllm <omarallam@protonmail.com>
2019-01-21 15:40:54 +0100

    Fix indentation

--- src/html.js
+++ src/html.js
@@ -15,12 +15,12 @@ export default class HTML extends React.Component {
           {this.props.headComponents}
           {`<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>`}
              {` <script>
-                (adsbygoogle = window.adsbygoogle || []).push({
-                  google_ad_client: "ca-pub-1540853335472527",
-                  enable_page_level_ads: true
-                });
-              </script> 
-            `}
+                  (adsbygoogle = window.adsbygoogle || []).push({
+                    google_ad_client: "ca-pub-1540853335472527",
+                    enable_page_level_ads: true
+                  });
+                </script> 
+              `}
         </head>
         <body {...this.props.bodyAttributes}>
           {this.props.preBodyComponents}
