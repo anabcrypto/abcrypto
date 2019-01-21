b795ff9c59023fceee7eefa3b9b3550f5e9de0ae
omrllm <omarallam@protonmail.com>
2019-01-21 17:48:16 +0100

    Fix Adsense error, but for real

--- src/html.js
+++ src/html.js
@@ -13,14 +13,15 @@ export default class HTML extends React.Component {
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
           />
           {this.props.headComponents}
-          {`<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>`}
-             {` <script>
-                  (adsbygoogle = window.adsbygoogle || []).push({
-                    google_ad_client: "ca-pub-1540853335472527",
-                    enable_page_level_ads: true
-                  });
-                </script> 
-              `}
+          <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+             <script>
+                  {`
+                    (adsbygoogle = window.adsbygoogle || []).push({
+                      google_ad_client: "ca-pub-1540853335472527",
+                      enable_page_level_ads: true
+                    });
+                  `}
+             </script>
         </head>
         <body {...this.props.bodyAttributes}>
           {this.props.preBodyComponents}
